41d64304384da864221b0c752e174e6459374928
kamal <kamal@kamal>
2025-03-14 05:59:03 +0000

    bedrock added

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import os
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_aws import ChatBedrockConverse
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-pro",
@@ -14,6 +15,10 @@ llm = ChatGoogleGenerativeAI(
     base_url="https://generativelanguage.googleapis.com/v1beta"
 )
 
+llm_bedrock = ChatBedrockConverse(model="meta.llama3-8b-instruct-v1:0",
+    temperature=0,
+    max_tokens=None,)
+
 messages = [
     (
         "system",
@@ -21,5 +26,5 @@ messages = [
     ),
     ("human", "I love programming and india."),
 ]
-ai_msg = llm.invoke(messages)
+ai_msg = llm_bedrock.invoke(messages)
 print(ai_msg)
